d72141c1ff61b7d0eaf1af9b4f7c06c30b873716
RTae <potae02@gmail.com>
2020-11-16 09:54:39 +0700

    Add lab

---
+++ receipt/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Product
+from .models import Customer
+from .models import Invoice
+from .models import InvoiceLineItem
+from .models import Receipt
+from .models import ReceiptLineItem
+
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Invoice)
+admin.site.register(InvoiceLineItem)
+admin.site.register(Receipt)
+admin.site.register(ReceiptLineItem)
